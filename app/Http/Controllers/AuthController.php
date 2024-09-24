6f6c8f743a0da6637da74c2b171dd6b55b1b654e
Omar Ayman <omaraymn411@gmail.com>
2024-09-24 14:32:06 +0300

    SuperApiApp Is here..!

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreUserRequest;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login()
+    {
+        return 'This is my login Method';
+    }
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken,
+        ]);
+    }
+    public function logout()
+    {
+        return response()->json('This is my logout Method');
+    }
+}
